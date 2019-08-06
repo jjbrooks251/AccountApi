4914ae83be181210a4d61328f80a4591476c808d
jjbrooks251 <jjbrooks251@outlook.com>
2019-08-06 16:20:42 +0100

    number gen working

--- charGen/src/main/java/com/qa/CharGenApplication.java
+++ charGen/src/main/java/com/qa/CharGenApplication.java
@@ -2,13 +2,12 @@ package com.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class NumGenApplication {
+public class CharGenApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ac = SpringApplication.run(NumGenApplication.class, args);
+		SpringApplication.run(CharGenApplication.class, args);
 	}
 
 }
